4865387c3a4643d7b5a580ec8342bbd37ddf981e
marchewadm <merchwa.dawid@gmail.com>
2024-05-14 10:08:35 +0200

    [feat]: add cors middleware, create new constant and update readme file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .users.router import router as users_router
 from .auth.router import router as auth_router
+from .constants import ALLOWED_ORIGIN
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ALLOWED_ORIGIN],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users_router)
 app.include_router(auth_router)
