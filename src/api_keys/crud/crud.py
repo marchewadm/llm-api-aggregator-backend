08b26139c2bbf95541dde78430a31195f0f34950
marchewadm <merchwa.dawid@gmail.com>
2024-06-05 18:18:13 +0200

    [refactor]: put all models inside database/models.py file, create alembic migration for this change

--- src/api_keys/crud/crud.py
+++ src/api_keys/crud/crud.py
@@ -37,6 +37,28 @@ def get_api_keys_by_user_id(db: Session, user_id: int) -> GetApiKeysResponse:
     return GetApiKeysResponse(api_key_models)
 
 
+def get_ai_models_by_user_id(db: Session, user_id: int) -> list[str]:
+    """
+    Retrieves all AI models from the database based on the user's ID.
+
+    Args:
+        db (Session): The database session.
+        user_id (int): The user's ID.
+
+    # Returns:
+    #     list[str]: A list of AI models.
+    """
+
+    ai_models = (
+        db.query(ApiKey.ai_model)
+        .filter(ApiKey.user_id == user_id)  # noqa
+        .order_by(ApiKey.id.asc())
+        .all()
+    )
+
+    return [ai_model[0] for ai_model in ai_models]
+
+
 def update_api_keys_by_user_id(
     db: Session, user_id: int, api_keys: list[ApiKeySchema]
 ) -> UpdateApiKeyResult:
