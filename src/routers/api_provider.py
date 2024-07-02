80aa7f6963df580e2c662f1e805a2aabe9dcf5b8
marchewadm <merchwa.dawid@gmail.com>
2024-07-02 15:04:32 +0200

    fix: order of /api-provider/all endpoint

--- src/routers/api_provider.py
+++ src/routers/api_provider.py
@@ -18,21 +18,33 @@ async def create_api_provider(
     payload: ApiProviderCreate,
     api_provider_service: ApiProviderServiceDependency,
 ):
+    """
+    Create a new API provider.
+    """
+
     return api_provider_service.create(payload)
-
-
-@router.get("/{api_provider_id}", response_model=ApiProviderResponse)
-async def get_api_provider_by_id(
-    api_provider_id: int, api_provider_service: ApiProviderServiceDependency
-):
-    return api_provider_service.get_one_by_id(api_provider_id)
 
 
 @router.get("/all", response_model=ApiProvidersResponse)
 async def get_api_providers_names(
     api_provider_service: ApiProviderServiceDependency,
 ):
+    """
+    Get all API providers names.
+    """
+
     return api_provider_service.get_all()
+
+
+@router.get("/{api_provider_id}", response_model=ApiProviderResponse)
+async def get_api_provider_by_id(
+    api_provider_id: int, api_provider_service: ApiProviderServiceDependency
+):
+    """
+    Get full information about an API provider by its ID.
+    """
+
+    return api_provider_service.get_one_by_id(api_provider_id)
 
 
 # TODO: Create permissions dependency for this endpoint to allow only admin users to delete API providers
@@ -40,4 +52,8 @@ async def get_api_providers_names(
 async def delete_api_provider(
     api_provider_id: int, api_provider_service: ApiProviderServiceDependency
 ):
+    """
+    Delete an API provider by its ID.
+    """
+
     return api_provider_service.delete_by_id(api_provider_id)
